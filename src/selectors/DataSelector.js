7ad89fde76b3df74caa2dab3c847b69cd8b0dc9a
rafaelfbs <rafael.ferrari.bs@gmail.com>
2018-04-15 21:52:29 -0300

    updated data selector algorithm to tokenizer/parser

--- src/selectors/DataSelector.js
+++ src/selectors/DataSelector.js
@@ -1,88 +1,189 @@
 
-const SELECTOR_REGEX = /(([.#]?)(\w+)(\[])?)/g;
-
 export class DataSelector {
     select(data, selector) {
-        let dataSelector = selector;
+        if (!selector) return data;
 
-        if (!dataSelector) {
-            return data;
-        }
+        const context = { pos: 0, selector };
+        const node = this.parseSelection(context);
 
-        if (!this.isPropertyAccessor(selector[0]) && !this.isMethodAccessor(selector[0])) {
-            dataSelector = '.' + dataSelector;
-        }
-
-        const matches = this.getMatches(dataSelector);
-        const parentMatch = ['root'];
-
-        if (!matches.length) {
-            throw new SyntaxError(`Wrong selector syntax in "${selector}"`);
-        }
-        
-        return matches.reduce((data, match) => {
-            const [cmd, accessor, path, modifier] = match.slice(1);
-            const absolutePath = parentMatch.join('');
-            
-            parentMatch.push(`${accessor}${path}${modifier || ''}`);
-            
-            if (this.isPropertyAccessor(accessor)) return this.getProperty(data, path, absolutePath, modifier);
-            if (this.isMethodAccessor(accessor)) return this.getMethod(data, path, absolutePath, modifier);
-
-            // TODO: fix regex matcher to match from start of string
-            throw new SyntaxError(`Wrong selector syntax in "${cmd}"`);
+        return node.entries.reduce((data, entry) => {
+            if (entry.type === 'property') return this.selectProperty(data, entry, selector);
+            if (entry.type === 'method') return this.selectMethod(data, entry, selector);
+            throw new SyntaxError(`Wrong selector syntax in "${selector.substring(entry.start, entry.end)}"`);
         }, data);
     }
-    
-    isPropertyAccessor(accessor) {
-        return accessor === '.';
-    }
-    
-    isMethodAccessor(accessor) {
-        return accessor === '#';
-    }
 
-    isFlatMapModifier(modifier) {
-        return modifier === '[]'
-    }
-    
-    getProperty(data, path, absolutePath, modifier) {
-        if (this.isFlatMapModifier(modifier)) {
+    selectProperty(data, entry, selector) {
+        if (entry.modifier && entry.modifier.type === 'flat-map-modifier') {
             if (!Array.isArray(data)) {
-                throw new SyntaxError(`Data is not an array for flat map property (${absolutePath})`);
+                throw new SyntaxError(`Data is not an array for flat map property (${selector.substring(entry.start, entry.end)})`);
             }
 
-            return this.reduceData(data, path, absolutePath, this.getProperty.bind(this));
+            return this.reduceData(data, entry, selector, this.getProperty.bind(this));
         }
-        if (!(path in data)) throw new SyntaxError(`No property "${path}" in data (${absolutePath})`);
-        return data[path];
+
+        return this.getProperty(data, entry, selector);
     }
-    
-    getMethod(data, path, absolutePath, modifier) {
-        if (this.isFlatMapModifier(modifier)) {
+
+    getProperty(data, entry, selector) {
+        const { value: path } = entry;
+
+        if (path in data) {
+            return data[path];
+        }
+
+        throw new SyntaxError(`No property "${path}" in data (${selector.substring(entry.start, entry.end)})`);
+    }
+
+    selectMethod(data, entry, selector) {
+        if (entry.modifier && entry.modifier.type === 'flat-map-modifier') {
             if (!Array.isArray(data)) {
-                throw new SyntaxError(`Data is not an array for flat map property (${absolutePath})`);
+                throw new SyntaxError(`Data is not an array for flat map property (${selector.substring(entry.start, entry.end)})`);
             }
 
-            return this.reduceData(data, path, absolutePath, this.getMethod.bind(this));
+            return this.reduceData(data, entry, selector, this.getMethod.bind(this));
         }
-        if (!(path in data)) throw new SyntaxError(`No method "${path}" in data (${absolutePath})`);
-        if (typeof data[path] !== 'function') throw new SyntaxError(`Property "${path}" is not a function (${absolutePath})`);
-        return data[path]();
+
+        return this.getMethod(data, entry, selector);
     }
 
-    reduceData(data, path, absolutePath, reducer) {
-        return data.reduce((arr, item) => arr.concat(reducer(item, path, absolutePath)), []);
-    }
-    
-    getMatches(selector) {
-        const matches = [];
-        let match;
-        
-        while ((match = SELECTOR_REGEX.exec(selector)) !== null) {
-            matches.push(match);
+    getMethod(data, entry, selector) {
+        const { value: path } = entry;
+
+        if (path in data) {
+            if (typeof data[path] === 'function') {
+                return data[path]();
+            }
+
+            throw new SyntaxError(`Property "${path}" is not a function (${selector.substring(entry.start, entry.end)})`);
         }
-        
-        return matches;
+
+        throw new SyntaxError(`No method "${path}" in data (${selector.substring(entry.start, entry.end)})`);
+    }
+
+    reduceData(data, entry, selector, reducer) {
+        return data.reduce((arr, item) => arr.concat(reducer(item, entry, selector)), []);
+    }
+
+    parseSelection(context) {
+        const len = context.selector.length;
+        const entries = [];
+
+        while (context.pos < len) {
+            entries.push(this.parseIdentifierWithAccessor(context));
+        }
+
+        return { type: 'selection', entries };
+    }
+
+    parseIdentifierWithAccessor(context) {
+        const accessor = this.nextToken(context);
+
+        if (accessor.type === 'property' || accessor.type === 'method') {
+            this.consumeToken(context);
+            return this.parseIdentifier(context, accessor);
+        } else if (accessor.type === 'identifier') {
+            return this.parseIdentifier(context, { type: 'property', start: accessor.start, end: accessor.start });
+        }
+
+        this.raiseException(context, accessor.start);
+    }
+
+    parseIdentifier(context, accessor) {
+        const identifier = this.consumeToken(context);
+
+        if (identifier.type === 'identifier') {
+            const modifier = this.parseModifier(context);
+            return {
+                type: accessor.type,
+                value: identifier.value,
+                modifier,
+                start: accessor.start,
+                end: modifier ? modifier.end : identifier.end
+            };
+        }
+
+        this.raiseException(context, identifier.start);
+    }
+
+    parseModifier(context) {
+        const modifierStart = this.nextToken(context);
+
+        if (modifierStart.type === 'bracket-start') {
+            const modifierEnd = this.consumeAndNextToken(context);
+
+            if (modifierEnd.type === 'bracket-end') {
+                this.consumeToken(context);
+                return { type: 'flat-map-modifier' };
+            }
+
+            this.raiseException(context, modifierEnd.start);
+        }
+
+        return null;
+    }
+
+    raiseException(context, position) {
+        throw new Error(`Unexpected token "${context.selector.charAt(position)}" at position "${position}"`);
+    }
+
+    isIdentifierChar(char) {
+        return (char >= 'a' && char <= 'z') ||
+            (char >= 'A' && char <= 'Z') ||
+            (char >= '0' && char <= '9');
+    }
+
+    consumeToken(context) {
+        const token = this.nextToken(context);
+        context.pos = token.end;
+        return token;
+    }
+
+    nextToken(context) {
+        const { pos, selector } = context;
+
+        return pos < selector.length
+            ? this.parseToken(context, selector.charAt(pos))
+            : { type: 'eof', start: pos, end: pos };
+    }
+
+    consumeAndNextToken(context) {
+        this.consumeToken(context);
+        return this.nextToken(context);
+    }
+
+    parseToken(context, char) {
+        switch (char) {
+            case '.': return { type: 'property', start: context.pos, end: context.pos + 1 };
+            case '#': return { type: 'method', start: context.pos, end: context.pos + 1 };
+            case '[': return { type: 'bracket-start', start: context.pos, end: context.pos + 1 };
+            case ']': return { type: 'bracket-end', start: context.pos, end: context.pos + 1 };
+            default:
+                if (this.isIdentifierChar(char)) return this.parseIdentifierToken(context);
+                throw new Error(`Unexpected token "${char}" at position "${context.pos}"`);
+        }
+    }
+
+    parseIdentifierToken(context) {
+        let pos = context.pos;
+        let selector = context.selector;
+        let len = selector.length;
+
+        while (pos < len) {
+            const char = selector.charAt(pos);
+
+            if (!this.isIdentifierChar(char)) {
+                break;
+            }
+
+            pos++;
+        }
+
+        return {
+            type: 'identifier',
+            value: context.selector.substring(context.pos, pos),
+            start: context.pos,
+            end: pos
+        };
     }
 }
