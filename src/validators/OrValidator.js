376ebe98be05ee6f17c266d654fb62d836a4a2ac
rafaelfbs <rafael.ferrari.bs@gmail.com>
2018-11-15 22:55:46 -0200

    refactor validators of type Rules

--- src/validators/OrValidator.js
+++ src/validators/OrValidator.js
@@ -1,7 +1,7 @@
 import { RulesValidator } from "./base/RulesValidator";
 
 export class OrValidator extends RulesValidator {
-    validate(data, rule) {
-        return this.getRules(rule).some(rule => this.validateRule(rule, data));
+    validateSelected(selected, predicate) {
+        return selected.some(predicate);
     }
 }
