ce060610f613bb3f3a1273d41ac400c14ede35ab
rafaelfbs <rafael.ferrari.bs@gmail.com>
2018-11-15 22:55:46 -0200

    refactor validators of type Rules

--- src/validators/AndValidator.js
+++ src/validators/AndValidator.js
@@ -1,7 +1,7 @@
 import { RulesValidator } from "./base/RulesValidator";
 
 export class AndValidator extends RulesValidator {
-    validate(data, rule) {
-        return this.getRules(rule).every(rule => this.validateRule(rule, data));
+    validateSelected(selected, predicate) {
+        return selected.every(predicate);
     }
 }
--- src/validators/base/RulesValidator.js
+++ src/validators/base/RulesValidator.js
@@ -8,4 +8,11 @@ export class RulesValidator extends BaseValidator {
     validateRule(rule, data) {
         return this.ruleTree.getValidator(rule.condition).validate(data, rule);
     }
+
+    validate(data, rule) {
+        return this.validateSelected(
+            this.getRules(rule),
+            rule => this.validateRule(rule, data)
+        );
+    }
 }
